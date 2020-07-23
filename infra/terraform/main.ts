d507c1e820dc65b02538a2d89a249092ddcc6d2a
Marc Bojoly <mbojoly@financeactive.com>
2020-07-23 11:34:59 +0000

    feat: Create first HTML page on AWS S3

--- infra/terraform/main.ts
+++ infra/terraform/main.ts
@@ -1,7 +1,8 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
 import { AwsProvider } from './.gen/providers/aws/aws-provider'
-import { S3Bucket } from './.gen/providers/aws/s3-bucket';
+import { S3Bucket, S3BucketConfig } from './.gen/providers/aws/s3-bucket';
+import { S3BucketObject, S3BucketObjectConfig } from './.gen/providers/aws/s3-bucket-object';
 
 class ProgrammezCloudAppStack extends TerraformStack {
   AWS_REGION = 'eu-west-3';
@@ -14,7 +15,23 @@ class ProgrammezCloudAppStack extends TerraformStack {
       region: this.AWS_REGION
     });
 
-    new S3Bucket(this, this.AWS_S3_BUCKET_NAME)
+    const s3BucketConfig: S3BucketConfig =  {
+      website: [{
+        indexDocument: 'index.html',
+        errorDocument: 'index.html'
+      }],
+      tags: { 'stack':'programmez-cloudapp-stack' }
+    };
+    const s3Bucket = new S3Bucket(this, this.AWS_S3_BUCKET_NAME, s3BucketConfig);
+    const s3ObjectConfig: S3BucketObjectConfig = { 
+      key: 'index.html', 
+      source: '../../../hello-cloud-app/index.html',
+      contentType: 'text/html',
+      acl: 'public-read',
+      bucket: s3Bucket.bucket ?? this.AWS_S3_BUCKET_NAME,
+      tags: { 'stack':'programmez-cloudapp-stack' }
+    } 
+    new S3BucketObject(this, 'index-page', s3ObjectConfig);
   }
 }
 
