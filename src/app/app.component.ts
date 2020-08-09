de913de495e0f3b8dd531713bc2f7a736cff9916
Marc Bojoly <mbojoly@financeactive.com>
2020-08-09 16:40:14 +0200

    feat: Refactor by introducting a service layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,61 @@
 import { Component, Input } from '@angular/core';
 
-enum PaymentType {
+export enum PaymentType {
   DRAWING = 'DRAWING',
   INTEREST = 'INTEREST',
   AMORTIZATION = 'AMORTIZATION'
 }
 
-interface Schedule {
+export interface Schedule {
   date: Date
   type: PaymentType
   amount: number
+}
+
+export interface Deal {
+  nominal:number;
+  monthsDuration:number;
+  interestRate:number;
+}
+
+export class ScheduleEngineService {
+
+  computeAmortization(nominal:number, monthsDuration:number) : number {
+    return nominal / monthsDuration;
+  }
+
+  computeInteresteAmount(principal:number, interestRate:number, periodNbOfMonths: number) : number {
+    return principal * interestRate/100 * periodNbOfMonths * 30/360
+  }
+
+  computeSchedule(deal: Deal): Schedule[] {
+    console.log(` ComputeSchedule with nominal ${deal.nominal}, duration ${deal.monthsDuration}, interest rate ${this.interestRate}`)
+    let schedule:Schedule[] = []
+    const startDate:Date = new Date(Date.now())
+    let principal:number = deal.nominal
+    schedule = schedule.concat({ 
+      "date": startDate,
+      "type": PaymentType.DRAWING,
+      "amount": -1*deal.nominal 
+    })
+    for(let i:number = 1; i < deal.monthsDuration; i++) {
+      const paymentDate:Date = new Date(startDate.getFullYear(), startDate.getMonth()+i, startDate.getDay())
+      const interestAmount:number = this.computeInteresteAmount(principal, deal.interestRate, 1)
+      schedule = schedule.concat({ 
+        "date": paymentDate,
+        "type": PaymentType.INTEREST,
+        "amount": interestAmount 
+      })
+      const amortization:number = this.computeAmortization(deal.nominal, deal.monthsDuration)
+      schedule = schedule.concat({ 
+        "date": paymentDate,
+        "type": PaymentType.AMORTIZATION,
+        "amount": amortization
+      })
+      principal = principal - amortization
+    }
+    return schedule
+  }
 }
 
 @Component({
@@ -26,48 +72,16 @@ export class AppComponent {
   interestRate:number;
   schedulePayments:Schedule[] = []
 
-  updateModel(): void {
+  clearModel(): void {
     this.schedulePayments = []
   }
 
-  updateSchedule():void {
-    this.schedulePayments = this.computeSchedule();
-  }
-
-  computeAmortization(nominal:number, monthsDuration:number) : number {
-    return nominal / monthsDuration;
-  }
-
-  computeInteresteAmount(principal:number, interestRate:number, periodNbOfMonths: number) : number {
-    return principal * interestRate/100 * periodNbOfMonths * 30/360
-  }
-
-  computeSchedule(): Schedule[] {
-    console.log(` ComputeSchedule with nominal ${this.nominal}, duration ${this.monthsDuration}, interest rate ${this.interestRate}`)
-    let schedule:Schedule[] = []
-    const startDate:Date = new Date(Date.now())
-    let principal:number = this.nominal
-    schedule = schedule.concat({ 
-      "date": startDate,
-      "type": PaymentType.DRAWING,
-      "amount": -1*this.nominal 
-    })
-    for(let i:number = 1; i < this.monthsDuration; i++) {
-      const paymentDate:Date = new Date(startDate.getFullYear(), startDate.getMonth()+i, startDate.getDay())
-      const interestAmount:number = this.computeInteresteAmount(principal, this.interestRate, 1)
-      schedule = schedule.concat({ 
-        "date": paymentDate,
-        "type": PaymentType.INTEREST,
-        "amount": interestAmount 
-      })
-      const amortization:number = this.computeAmortization(this.nominal, this.monthsDuration)
-      schedule = schedule.concat({ 
-        "date": paymentDate,
-        "type": PaymentType.AMORTIZATION,
-        "amount": amortization
-      })
-      principal = principal - amortization
-    }
-    return schedule
+  renderSchedule():void {
+    const service:ScheduleEngineService = new ScheduleEngineService()
+    this.schedulePayments = service.computeSchedule({
+      nominal:this.nominal,
+      monthsDuration:this.monthsDuration,
+      interestRate:this.interestRate
+    });
   }
 }
