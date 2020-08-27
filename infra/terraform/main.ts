05c47d0163fe053c6dae33c4fc2f5f59ee1c2b37
Marc Bojoly <mbojoly@financeactive.com>
2020-08-27 18:52:21 +0000

    Upgrade CDKTF and upload many files

--- infra/terraform/main.ts
+++ infra/terraform/main.ts
@@ -3,6 +3,7 @@ import { App, TerraformStack } from 'cdktf';
 import { AwsProvider } from './.gen/providers/aws/aws-provider'
 import { S3Bucket, S3BucketConfig } from './.gen/providers/aws/s3-bucket';
 import { S3BucketObject, S3BucketObjectConfig } from './.gen/providers/aws/s3-bucket-object';
+import * as fs from 'fs';
 
 class ProgrammezCloudAppStack extends TerraformStack {
   AWS_REGION = 'eu-west-3';
@@ -23,15 +24,25 @@ class ProgrammezCloudAppStack extends TerraformStack {
       tags: { 'stack':'programmez-cloudapp-stack' }
     };
     const s3Bucket = new S3Bucket(this, this.AWS_S3_BUCKET_NAME, s3BucketConfig);
-    const s3ObjectConfig: S3BucketObjectConfig = { 
-      key: 'index.html', 
-      source: '../../../hello-cloud-app/index.html',
-      contentType: 'text/html',
-      acl: 'public-read',
-      bucket: s3Bucket.bucket ?? this.AWS_S3_BUCKET_NAME,
-      tags: { 'stack':'programmez-cloudapp-stack' }
-    } 
-    new S3BucketObject(this, 'index-page', s3ObjectConfig);
+
+    const deployDirectory = '../../hello-cloud-app/'
+    fs.readdir(deployDirectory, (err, files) => {
+      if(!err) {
+        for(let file of files) {
+          console.log('file ' + file)
+          const s3ObjectConfig: S3BucketObjectConfig = { 
+            key: file, 
+            source: deployDirectory + file,
+            acl: 'public-read',
+            bucket: s3Bucket.bucket ?? this.AWS_S3_BUCKET_NAME,
+            tags: { 'stack':'programmez-cloudapp-stack' }
+          } 
+          new S3BucketObject(this, file, s3ObjectConfig);
+        }
+      } else {
+        throw new Error('Error (' + err +  ') reading deployment directory ' + deployDirectory);
+      }
+    });
   }
 }
 
