9846d2a760af06b59d896bbe4be0d5081088a372
Marc Bojoly <mbojoly@financeactive.com>
2020-12-13 15:48:12 +0100

    fix: comply with esLint

--- hello-cloud-app/src/app/app.component.ts
+++ hello-cloud-app/src/app/app.component.ts
@@ -1,26 +1,26 @@
-import { Component } from '@angular/core'; //1
-import { Schedule, computeSchedule } from './schedule-engine.service' //2
+import { Component } from '@angular/core'; // 1
+import { Schedule, computeSchedule } from './schedule-engine.service'; // 2
 
-@Component({ //3
+@Component({ // 3
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nominal:number; //4
-  monthsDuration:number;
-  interestRate:number;
-  schedulePayments:Schedule[] = []
+  nominal: number; // 4
+  monthsDuration: number;
+  interestRate: number;
+  schedulePayments: Schedule[] = [];
 
-  clearModel(): void { //5
-    this.schedulePayments = []
+  clearModel(): void { // 5
+    this.schedulePayments = [];
   }
 
-  renderSchedule():void { //6
+  renderSchedule(): void { // 6
     this.schedulePayments = computeSchedule({
-      nominal:this.nominal,
-      monthsDuration:this.monthsDuration,
-      interestRate:this.interestRate
+      nominal: this.nominal,
+      monthsDuration: this.monthsDuration,
+      interestRate: this.interestRate
     });
   }
 }
